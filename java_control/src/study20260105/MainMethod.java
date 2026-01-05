2092b20faa6be26bab2f285cb0bedfd21724adfb
dw-029 <dw-029@DESKTOP-9DIUOBQ>
2026-01-05 10:33:56 +0900

    Merge branch 'main' of https://github.com/jiyeon12191-dev/smile_java.git into main

--- java_control/src/study20260105/MainMethod.java
+++ java_control/src/study20260105/MainMethod.java
@@ -3,7 +3,10 @@ package study20260105;
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double  num = Math.random();
+		
+		System.out.println(num);
 
 	}
 
