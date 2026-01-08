a9e529758772b5a8b5afa3a5bb7a21d3fdc2b39d
DW <DW@DESKTOP-SIN4VTM>
2026-01-08 17:31:09 +0900

    20260108 생성자 메서드

---
+++ java_control/src/study20260108/JavaClass2.java
@@ -0,0 +1,32 @@
+package study20260108;
+
+public class JavaClass2 {
+
+	public static void main(String[] args) {
+	
+		Movie movie = new Movie();
+		// new 	Movie() 생성자 메서드
+		
+		System.out.println(movie.title);
+		
+		movie.title="아바타 불과재";
+		movie.dir="카메론";
+		movie.year=2026;
+		movie.limitAge=12;
+		System.out.println(movie.title);
+		
+		
+		Movie movie2 = new Movie("굿포춘",2026);		
+		
+		System.out.println(movie2.title);
+		System.out.println(movie2.year);
+		
+		// Music 클래스의 객체 생성하여
+	    // 음악제목, 가수,재생시간(총몇분) 저장되게 하기
+		// 매개변수 있는 생성자 메서드로 데이터 저장하고 
+		// 출력하기 
+		
+
+	}
+
+}
---
+++ java_control/src/study20260108/Music.java
@@ -0,0 +1,27 @@
+package study20260108;
+
+public class Music {
+	String MusicName;
+    String singer;
+    int time;
+    
+	
+	Music(String MusicName,String singer,int time){
+	    this. MusicName=MusicName;
+	    
+	    System.out.println("Music.name");
+	    }
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
