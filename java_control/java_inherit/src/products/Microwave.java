ac76b19c64c823ab21977df5ff7c905a51723186
DW <DW@DESKTOP-SIN4VTM>
2026-01-15 08:50:02 +0900

    20260114

---
+++ java_control/java_inherit/src/products/Microwave.java
@@ -0,0 +1,11 @@
+package products;
+
+import common.Appliance;
+
+public class Microwave  extends Appliance{ // 전자레인지
+
+	
+	
+	@Override
+	public void power() {}
+}
--- java_control/java_inherit/src/products/Mycrowave.java
+++
@@ -1,7 +0,0 @@
-package products;
-
-import common.Appliance;
-
-public class Mycrowave  extends Appliance{ // 전자레인지
-
-}
